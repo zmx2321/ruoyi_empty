679de2aa54a20276ba25122dfea05d02e9f1b271
zmx2321 <zmx2321@163.com>
2022-06-14 14:02:52 +0800

    deploy

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 }
 const name = process.env.VUE_APP_TITLE || '管理系统' // 网页标题
 
-const port = process.env.port || process.env.npm_config_port || 8088 // 端口
+const port = process.env.port || process.env.npm_config_port || 8099 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
